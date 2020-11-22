85f24e6a6e0ef1ccbe2d8e58a37f8338ee72025c
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-11-22 22:19:41 +0200

    added catalog page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./containers/Header/Header";
 import Footer from "./containers/Footer/Footer";
+import Catalog from "./pages/Catalog";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Home />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/catalog">
+          <Catalog />
+        </Route>
+      </Switch>
+
       <Footer />
     </Router>
   );
