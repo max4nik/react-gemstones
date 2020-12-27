281e6a8d81e59f64d0185bda710d369894922570
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-12-27 23:38:12 +0200

    added login components

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginContainer from "../containers/Login/LoginContainer";
+
+const Login = () => {
+  return (
+    <>
+      <LoginContainer />
+    </>
+  );
+};
+
+export default Login;
