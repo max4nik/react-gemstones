8b7bf2f0f6a611c08f2bca42dfec722941ab2c6d
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-12-27 23:40:11 +0200

    added protected routers implementation

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Nav,
   NavbarContainer,
@@ -8,44 +8,67 @@ import {
   UserOptions,
 } from "./Header.styled";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import PersonIcon from "@material-ui/icons/Person";
+import LogoutIcon from "@material-ui/icons/ExitToApp";
 import { GemLogo } from "../../components/Logo/Logo.styled";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const logoutUser = () => {
+    localStorage.removeItem("email-local");
+    localStorage.removeItem("password-local");
+    setIsLogged(false);
+  };
+  useEffect(() => {
+    checkLogged();
+    console.log();
+  }, [isLogged]);
+  const checkLogged = () => {
+    const username = localStorage.getItem("email-local");
+    const password = localStorage.getItem("password-local");
+    if (username != null && password != null) {
+      if (isLogged == false) {
+        setIsLogged(true);
+      }
+    }
+  };
+
   return (
     <>
       <Nav>
         <NavbarContainer>
           <GemLogo to="/">GemsS</GemLogo>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to="/">Home</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="/catalog">Catalog</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="/about">About Us</NavLinks>
-            </NavItem>
-          </NavMenu>
-          <UserOptions>
-            <Link to="/cart">
-              <ShoppingCartIcon
-                style={{ fontSize: 40, color: "white", cursor: "pointer" }}
-              />
-            </Link>
-            <Link to="/user">
-              <PersonIcon
-                style={{
-                  fontSize: 40,
-                  color: "white",
-                  marginLeft: 30,
-                  cursor: "pointer",
-                }}
-              />
-            </Link>
-          </UserOptions>
+          {isLogged ? (
+            <>
+              <NavMenu>
+                <NavItem>
+                  <NavLinks to="/">Home</NavLinks>
+                </NavItem>
+                <NavItem>
+                  <NavLinks to="/catalog">Catalog</NavLinks>
+                </NavItem>
+              </NavMenu>
+              <UserOptions>
+                <Link to="/cart">
+                  <ShoppingCartIcon
+                    style={{ fontSize: 40, color: "white", cursor: "pointer" }}
+                  />
+                </Link>
+                <Link to="/login" onClick={logoutUser}>
+                  <LogoutIcon
+                    style={{
+                      fontSize: 40,
+                      color: "white",
+                      marginLeft: 30,
+                      cursor: "pointer",
+                    }}
+                  />
+                </Link>
+              </UserOptions>
+            </>
+          ) : (
+            <></>
+          )}
         </NavbarContainer>
       </Nav>
     </>
